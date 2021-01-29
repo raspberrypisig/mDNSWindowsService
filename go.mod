25fe853ba989690bb51b7554185970ed0b97d128
Raspberry Pi <raspberrypisig@gmail.com>
2021-01-29 11:01:31 +1100

    first commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module mdnsmohanservice
+
+go 1.14
+
+require (
+	github.com/grandcat/zeroconf v1.0.0
+	github.com/kardianos/service v1.2.0
+	github.com/miekg/dns v1.1.35 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
---
+++ main.go
@@ -0,0 +1,145 @@
+// Copyright 2015 Daniel Theophanes.
+// Use of this source code is governed by a zlib-style
+// license that can be found in the LICENSE file.
+
+// Simple service that only works by printing a log message every few seconds.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/grandcat/zeroconf"
+	"github.com/kardianos/service"
+)
+
+var logger service.Logger
+
+// Program structures.
+//  Define Start and Stop methods.
+type program struct {
+	exit chan struct{}
+}
+
+func (p *program) Start(s service.Service) error {
+	if service.Interactive() {
+		logger.Info("Running in terminal.")
+	} else {
+		logger.Info("Running under service manager.")
+	}
+	p.exit = make(chan struct{})
+
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+
+func connected() bool {
+	_, err := http.Get("http://example.com")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func (p *program) run() error {
+	/*
+		logger.Infof("I'm running %v.", service.Platform())
+		ticker := time.NewTicker(2 * time.Second)
+		for {
+			select {
+			case tm := <-ticker.C:
+				logger.Infof("Still running at %v...", tm)
+			case <-p.exit:
+				ticker.Stop()
+				return nil
+			}
+		}
+	*/
+
+	for !connected() {
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+	fmt.Println("Hello")
+	host, _ := os.Hostname()
+	server, err := zeroconf.Register(host, "_workstation._tcp", "local.", 42424, []string{"txtv=0", "lo=1", "la=2"}, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer server.Shutdown()
+
+	// Clean exit.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	select {
+	case <-sig:
+
+	}
+
+	log.Println("Shutting down.")
+
+	return nil
+}
+func (p *program) Stop(s service.Service) error {
+	// Any work in Stop should be quick, usually a few seconds at most.
+	logger.Info("I'm Stopping!")
+	close(p.exit)
+	return nil
+}
+
+// Service setup.
+//   Define service config.
+//   Create the service.
+//   Setup the logger.
+//   Handle service controls (optional).
+//   Run the service.
+func main() {
+	svcFlag := flag.String("service", "", "Control the system service.")
+	flag.Parse()
+
+	svcConfig := &service.Config{
+		Name:        "MDNSMohanService",
+		DisplayName: "MDNSMohanService",
+		Description: "mDNS server responds with _workstation._tcp queries ",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errs := make(chan error, 5)
+	logger, err = s.Logger(errs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			err := <-errs
+			if err != nil {
+				log.Print(err)
+			}
+		}
+	}()
+
+	if len(*svcFlag) != 0 {
+		err := service.Control(s, *svcFlag)
+		if err != nil {
+			log.Printf("Valid actions: %q\n", service.ControlAction)
+			log.Fatal(err)
+		}
+		return
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+}
